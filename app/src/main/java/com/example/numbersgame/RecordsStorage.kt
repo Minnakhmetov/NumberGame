f1190082cdae47ec537d0c4bd877d152a8645d13
Bulat <BulatMinnakh@gmail.com>
2020-07-20 17:57:26 +0300

    Records are saved using SharedPreferences and display in ChapterChoiceFragment

---
+++ app/src/main/java/com/example/numbersgame/RecordsStorage.kt
@@ -0,0 +1,12 @@
+package com.example.numbersgame
+
+import android.app.Application
+import android.content.Context
+
+class RecordsStorage(private val context: Context) {
+    fun saveRecord(chapterId: Int, record: Int) {
+    }
+    fun getRecord(chapterId: Int) {
+
+    }
+}
