f7cf6061713762eab1f947f8bd358f06fbb56aac
Bulat <BulatMinnakh@gmail.com>
2020-08-02 13:59:58 +0300

    divider color is in color.xml now

--- app/src/main/java/com/example/numbersgame/ChapterChoiceFragment.kt
+++ app/src/main/java/com/example/numbersgame/ChapterChoiceFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.numbersgame.databinding.FragmentChapterChoiceBinding
@@ -24,7 +25,14 @@ class ChapterChoiceFragment : Fragment() {
             )
         }
 
-        binding.chaptersList.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        binding.chaptersList.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL).apply {
+                context?.let { context ->
+                    ContextCompat.getDrawable(context, R.drawable.divider)?.let { setDrawable(it) }
+                }
+            }
+        )
+
         binding.chaptersList.adapter = adapter
 
         val recordsStorage =
