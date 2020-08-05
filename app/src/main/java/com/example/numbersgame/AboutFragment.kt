783b7f16a12afcaf939ef6d7537047d6d41a2fde
Bulat <BulatMinnakh@gmail.com>
2020-08-05 19:18:34 +0300

    Changed finished AboutFragment

--- app/src/main/java/com/example/numbersgame/AboutFragment.kt
+++ app/src/main/java/com/example/numbersgame/AboutFragment.kt
@@ -1,10 +1,16 @@
 package com.example.numbersgame
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.example.numbersgame.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
 
@@ -18,6 +24,21 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val binding = FragmentAboutBinding.inflate(inflater, container, false)
+
+        binding.creditsHeader.findViewById<TextView>(R.id.title).text = getString(R.string.credits_header)
+        binding.feedbackHeader.findViewById<TextView>(R.id.title).text = getString(R.string.feedback_header)
+
+        val createdByStyledString = SpannableString(getString(R.string.created_by))
+        createdByStyledString.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.colorSecondary)),
+            0,
+            10,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.createdByText.text = createdByStyledString
+
+        return binding.root
     }
 }
